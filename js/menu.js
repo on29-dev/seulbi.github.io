e8d7f04e0d5092828a3d4f0d60c7bc9f98290957
kw8799 <kw8799@gmail.com>
2017-09-22 14:21:31 +0900

    add and edit add css/stylesheet.css example/ js/menu.js edit index.html js/webcafe.js

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+// 내가 짠 코드
+// $(document).ready(function() {
+//   $('.btn-menubar').on('click', function() {
+//     $(this).parent().siblings('.main-menu').toggleClass('menu-active');
+//   })
+// });
+
+$(document).ready(function() {
+  var container = $('.menubar-container')
+  var btn_menubar = $('.btn-menubar, .menubar');
+  var btn = $('.btn-menubar');
+  var menu = $('.main-menu');
+
+  btn_menubar.on('click', function() {
+    menu.toggleClass('menu-act');
+    if (menu.hasClass('menu-act')) {
+      btn.text('메인메뉴 닫기');
+      // $('.menubar-top').toggleClass('menubar-top-clicked');
+      // $('.menubar-middle').toggleClass('menubar-middle-clicked');
+      // $('.menubar-bottom').toggleClass('menubar-bottom-clicked');
+    } else {
+      btn.text('메인메뉴 열기');
+      // $('.menubar-top-clicked').toggleClass('menubar-top');
+      // $('.menubar-middle-clicked').toggleClass('menubar-middle');
+      // $('.menubar-bottom-clicked').toggleClass('menubar-bottom');
+    }
+  });
+  btn_menubar.on('click', function() {
+    container.toggleClass('menubar-act');
+  });
+});
--- js/webcafe.js
+++ js/webcafe.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('.btn-menubar').on('click', function() {
     $(this).parent().toggleClass("menubar-act");
     $(this).parent().siblings().toggleClass('main-menu-act');
-    if($(this).parent().hasClass('menubar-act')) {
+    if ($(this).parent().hasClass('menubar-act')) {
       $(this).text('메인메뉴 닫기');
     } else {
       $(this).text('메인메뉴 열기');
@@ -11,7 +11,7 @@ $(document).ready(function() {
   $('.menubar').on('click', function() {
     $(this).parent().toggleClass("menubar-act");
     $(this).parent().siblings().toggleClass('main-menu-act');
-    if($(this).parent().hasClass('menubar-act')) {
+    if ($(this).parent().hasClass('menubar-act')) {
       $('.btn-menubar').text('메인메뉴 닫기');
     } else {
       $('.btn-menubar').text('메인메뉴 열기');
